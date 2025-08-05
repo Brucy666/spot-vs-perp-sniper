bdb18aa5f24f37a76fe4c316e4fdd2738add41dc
Brucy666 <robleach16@icloud.com>
2025-08-05 23:10:28 +0100

    Update swing_vs_perp_engine.py

--- swing_vs_perp_engine.py
+++ swing_vs_perp_engine.py
@@ -16,7 +16,7 @@ from utils.multi_tf_memory import MultiTFMemory
 from utils.spot_perp_alert_dispatcher import SpotPerpAlertDispatcher
 from utils.sniper_alert_logger import log_sniper_alert
 
-from volume_fetcher import fetch_all_volume
+from utils.volume_fetcher import fetch_all_volume
 from volume_scorer import score_volume_bias
 from scorer_swing import score_swing_confluence
 
